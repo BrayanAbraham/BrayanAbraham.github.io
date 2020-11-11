ab16241123d6ab65bdd0cce17ee7cdde4384ff13
BrayanAbraham <brayanabraham@gmail.com>
2020-11-11 21:01:23 +0530

    Grammar

--- src/components/content/about/Description.js
+++ src/components/content/about/Description.js
@@ -6,15 +6,15 @@ const Description = () => {
       <div className="container">
         <div className="text">
           <p>
-            Hello, I'm Brayan Abraham, a Software Enginner from New Delhi. I
-            specialise in building remarkable websites, applications and
-            everything in between. I recently completed B.Tech. in Computer
+            Hello, I'm Brayan Abraham, a Software Engineer from New Delhi. I
+            specialise in building great websites, applications and everything
+            in between. I recently completed my B.Tech. Degree in Computer
             Science &amp; Engineering from Maharaja Surajmal Institute of
             Technology (GGSIPU).
           </p>
           <p>
             I fell in love with coding when I was in 11th Standard in the
-            computer labs of St. Gregorios School and I haven't stopped since,
+            computer labs of St. Gregorios School, and I haven't stopped since,
             developing Full-Stack Web Applications as well as Stand-Alone
             Applications. I'm into competitive coding and sometimes write poetry
             and play Football.
