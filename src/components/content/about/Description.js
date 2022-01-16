162140660899f39e60e790f5e03fe537f1975972
BrayanAbraham <brayanabraham@gmail.com>
2022-01-16 10:12:16 +0530

    Change Description

--- src/components/content/about/Description.js
+++ src/components/content/about/Description.js
@@ -6,18 +6,16 @@ const Description = () => {
       <div className="container">
         <div className="text">
           <p>
-            Hello, I'm Brayan Abraham, a Software Engineer from New Delhi. I
-            specialise in building great websites, applications and everything
-            in between. I recently completed my B.Tech. Degree in Computer
-            Science &amp; Engineering from Maharaja Surajmal Institute of
-            Technology (GGSIPU).
+            Hello, I'm Brayan Abraham, a Software Engineer from New Delhi,
+            working at Wingify Sofware Pvt. Ltd. I specialise in building great
+            websites, applications and everything in between.
           </p>
           <p>
-            I fell in love with coding when I was in 11th Standard in the
-            computer labs of St. Gregorios School, and I haven't stopped since,
-            developing Full-Stack Web Applications as well as Stand-Alone
-            Applications. I'm into competitive coding and sometimes write poetry
-            and play Football.
+            I fell in love with coding when I was in 11th Standard in my
+            school's computer labs, and I haven't stopped since, developing
+            Full-Stack Web Applications as well as Stand-Alone Applications. I'm
+            into competitive coding and sometimes write poetry and play
+            Football.
           </p>
         </div>
       </div>
